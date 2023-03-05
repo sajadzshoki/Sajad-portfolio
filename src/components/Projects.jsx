dc85b376fa56f35348d447fc4ff1968682b911df
sajad shokraei <shockzbeatbox@gmail.com>
2023-03-05 13:24:07 +0330

    update (some bugs fixed and main project changed )

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,29 +2,34 @@ import React from "react";
 import ProjectCard from "./ProjectCard";
 import projectsData from "../projectsData";
 import { Link } from "react-router-dom";
-
+import mytones2 from "../images/mytones2.png";
+import iphone from "../images/iphone.png";
 
 const Projects = () => {
-  const projects = projectsData.map((item) => {
-    return (
-      <ProjectCard
-        key={item.id}
-        image={item.image}
-        title={item.title}
-        description={item.description}
-        githubLink={item.githubLink}
-        projectDemo={item.projectDemo}
-        techs={item.Technologies}
-      />
-    );
-  });
+  // const projects = projectsData.map((item) => {
+  //   return (
+  //     <ProjectCard
+  //       key={item.id}
+  //       image={item.image}
+  //       title={item.title}
+  //       description={item.description}
+  //       githubLink={item.githubLink}
+  //       projectDemo={item.projectDemo}
+  //       techs={item.Technologies}
+  //     />
+  //   );
+  // });
   return (
-    <div className="project" id="project">
-      <h1 className="projects-header header">My Recent Projects</h1>
-      
-        
-      <section className="Info-card">{projects}</section>
-      
+    <div className="main-project-container" id="project">
+      <h1 className="header">My Project</h1>
+      <div className="main-project">
+        <a href="https://sajadzshoki.github.io/Mytones/">
+          <img alt="project" src={mytones2} className="main-project-image" title="go to website" />
+        </a>
+        <a href="https://sajadzshoki.github.io/Mytones/">
+          <img alt="project" src={iphone} className="iphone" title="go to website"/>
+        </a>
+      </div>
       <Link to="/all_projects">
         <button className="btn all-projects-link">
           See all of my Projects
