8d72c0fb970b65f07f632365e5321a136a943fc1
sajad shokraei <shockzbeatbox@gmail.com>
2023-03-06 22:39:01 +0330

    update (style )

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,24 +1,10 @@
 import React from "react";
-import ProjectCard from "./ProjectCard";
-import projectsData from "../projectsData";
 import { Link } from "react-router-dom";
 import mytones2 from "../images/mytones2.png";
 import iphone from "../images/iphone.png";
 
 const Projects = () => {
-  // const projects = projectsData.map((item) => {
-  //   return (
-  //     <ProjectCard
-  //       key={item.id}
-  //       image={item.image}
-  //       title={item.title}
-  //       description={item.description}
-  //       githubLink={item.githubLink}
-  //       projectDemo={item.projectDemo}
-  //       techs={item.Technologies}
-  //     />
-  //   );
-  // });
+
   return (
     <div className="main-project-container" id="project">
       <h1 className="header">My Project</h1>
