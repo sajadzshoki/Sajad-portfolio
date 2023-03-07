e9147f26d3d8577496543cd10e3ccaa2fd81059f
sajad shokraei <shockzbeatbox@gmail.com>
2023-03-07 12:49:30 +0330

    update dropdown

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,16 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
 import { Link } from "react-router-dom";
-
+import projectsData from '../projectsData';
 const Navbar = () => {
-
-
-  const goToTop =()=>{
+  const goToTop = () => {
     window.scroll({
-      top: 0, 
-      behavior: 'smooth' 
-     });
-  }
+      top: 0,
+      behavior: "smooth",
+    });
+  };
 
-  return (                        
+  return (
     <nav className="navbar">
       <ul>
         <li onClick={goToTop}>
@@ -21,17 +19,37 @@ const Navbar = () => {
           </Link>
         </li>
         <li className="nav-skills">
-          <a href="/#skill" >MY SKILLS</a>
+          <a href="/#skill">MY SKILLS</a>
         </li>
-        <li>
+        <li className="dropdown">
           <a href="/#project">MY PROJECTS</a>
+          <ul className="dropdown-menu">
+            <li>
+              <a href="#">Youtube Clone</a>
+            </li>
+            <li>
+              <a href="#">budget manager</a>
+            </li>
+            <li>
+              <a href="#">mytones</a>
+            </li>
+            <li>
+              <a href="#">mytones</a>
+            </li>
+            <li>
+              <a href="#">mytones</a>
+            </li>
+            <li>
+              <a href="#">mytones</a>
+            </li>
+          </ul>
         </li>
         <li>
           <a href="/#about-me">ABOUT ME</a>
         </li>
         <li>
           <Link to="contact_me">
-          <a href="/#footer">CONTACT ME</a>
+            <a href="/#footer">CONTACT ME</a>
           </Link>
         </li>
       </ul>
