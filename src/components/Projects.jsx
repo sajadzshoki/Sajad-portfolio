f9e3a63deda1a04f7935e0106032050febf92327
sajad shokraei <shockzbeatbox@gmail.com>
2023-04-10 00:28:57 +0430

    projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,33 +3,36 @@ import { Link } from "react-router-dom";
 import mytones2 from "../images/mytones2.png";
 import youtube from "../images/youtube.png";
 import mui from "../images/mui.png";
-import iphone from "../images/iphone.png";
+import quizzical from "../images/quizzical.png";
+import mui_phone from "../images/mui_phone.png";
+import mytones_phone from "../images/mytones_phone.png";
+import quizzical_phone from "../images/quizzical_phone.png";
 
 const Projects = () => {
 
   const goRight =()=>{
-    document.getElementById('project').scrollLeft +=1100;
+    document.getElementById('project').scrollLeft +=1000;
     
   }
   const goLeft =()=>{
-    document.getElementById('project').scrollLeft -=1100;
+    document.getElementById('project').scrollLeft -=1000;
     
   }
   return (
     <div className="projects">
-      <button className="arrow-r btn" onClick={goRight} onScroll>
+      <button className="arrow-r btn" onClick={goRight}>
         <i
           class="bx bxs-chevron-right"
-          style={{ fontSize: "3rem", color: "var(--white)" }}
+          style={{color: "var(--white)" }}
         ></i>
       </button>
-      <button className="arrow-l btn" onClick={goLeft} onScroll>
+      <button className="arrow-l btn" onClick={goLeft} >
         <i
           class="bx bxs-chevron-left"
-          style={{ fontSize: "3rem", color: "var(--white)" }}
+          style={{ color: "var(--white)" }}
         ></i>
       </button>
-      <h1 className="header">My Project</h1>
+      <h1 className="header">My Projects</h1>
       <div className="main-project-container" id="project">
         <div className="main-project" >
           <a href="https://sajadzshoki.github.io/Mytones/">
@@ -43,7 +46,7 @@ const Projects = () => {
           <a href="https://sajadzshoki.github.io/Mytones/">
             <img
               alt="project"
-              src={iphone}
+              src={mytones_phone}
               className="iphone"
               title="go to website"
             />
@@ -51,26 +54,7 @@ const Projects = () => {
         </div>
 
         <div className="main-project">
-          <a href="https://sajadzshoki.github.io/Youtube-Clone/">
-            <img
-              alt="project"
-              src={youtube}
-              className="main-project-image"
-              title="go to website"
-            />
-          </a>
-          <a href="https://sajadzshoki.github.io/Mytones/">
-            <img
-              alt="project"
-              src={iphone}
-              className="iphone"
-              title="go to website"
-            />
-          </a>
-        </div>
-
-        <div className="main-project">
-          <a href="https://sajadzshoki.github.io/Youtube-Clone/">
+          <a href="https://socialmedia-react-materialui.netlify.app/">
             <img
               alt="project"
               src={mui}
@@ -78,17 +62,54 @@ const Projects = () => {
               title="go to website"
             />
           </a>
-          <a href="https://sajadzshoki.github.io/Mytones/">
+          <a href="https://socialmedia-react-materialui.netlify.app/">
             <img
               alt="project"
-              src={iphone}
+              src={mui_phone}
               className="iphone"
               title="go to website"
             />
           </a>
         </div>
+        <div className="main-project">
+          <a href="https://youtube-clone-sajad.vercel.app">
+            <img
+              alt="project"
+              src={youtube}
+              className="main-project-image"
+              title="go to website"
+            />
+          </a>
+          <a href="https://youtube-clone-sajad.vercel.app">
+            <img
+              alt="project"
+              src={mui_phone}
+              className="iphone"
+              title="go to website"
+            />
+          </a>
+        </div>
+        <div className="main-project">
+          <a href="https://quizzical-sajad.vercel.app">
+            <img
+              alt="project"
+              src={quizzical}
+              className="main-project-image"
+              title="go to website"
+            />
+          </a>
+          <a href="https://quizzical-sajad.vercel.app">
+            <img
+              alt="project"
+              src={quizzical_phone}
+              className="iphone"
+              title="go to website"
+            />
+          </a>
+        </div>
+
       </div>
-      <Link to="/all_projects">
+      <Link to="/all_projects" className="projects-btn">
         <button className="btn all-projects-link">
           See all of my Projects
         </button>
